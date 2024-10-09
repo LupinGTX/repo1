93cccf70c950766fa2f6f5a5cb08f7cc70744fdf
Lukasz Pniewski <323021@stud.umk.pl>
2024-10-09 17:25:53 +0200

    Final

--- plik4.c
+++ plik4.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-        printf("There's something inside me\nIt's hard to explain\nThey're talking about you boy\n");
+        printf("There's something inside me\nIt's hard to explain\nThey're talking about you boy\nBut you're still the same\n");
         return 0;
 }
